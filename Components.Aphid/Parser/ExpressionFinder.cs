607c34bd3eb02da4d1e60fc8f37e6b590052d128
John Leitch <JohnLeitch@outlook.com>
2015-10-04 00:09:51 -0400

    Added ExpressionFinder.

---
+++ Components.Aphid/Parser/ExpressionFinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Components.Aphid.Parser
+{
+    public class ExpressionFinder : AphidMutator
+    {
+        private List<AphidExpression> _expressions = new List<AphidExpression>();
+
+        private AphidExpressionType _type;
+
+        protected override List<AphidExpression> MutateCore(AphidExpression expression, out bool hasChanged)
+        {
+            hasChanged = false;
+
+            if (expression.Type == _type)
+            {
+                _expressions.Add(expression);
+            }
+
+            return null;
+        }
+
+        public AphidExpression[] Find(AphidExpressionType type, List<AphidExpression> ast)
+        {
+            _type = type;
+            MutateRecursively(ast);
+            Reset();
+            return _expressions.ToArray();
+        }
+    }
+}
