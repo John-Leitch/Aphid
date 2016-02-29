c980ceabd692f09cfdf131acc98183ba7a8690d0
John Leitch <JohnLeitch@outlook.com>
2016-02-28 20:47:44 -0800

    Removed code gen stuff.

--- Boxelder/Program.cs
+++ Boxelder/Program.cs
@@ -28,49 +28,5 @@ else print('Nope I guess');
             var python = emitter.Compile(AphidParser.Parse(_code));
             File.WriteAllText(@"C:\temp\test.py", python);
         }
-
-        private static string GenerateTokenMethods()
-        {
-            var fmt = "protected virtual void Emit{0}({0} expression) {{ " +
-                "throw new NotImplementedException(); }}";
-
-            var methods = Enum
-                .GetValues(typeof(AphidExpressionType))
-                .OfType<AphidExpressionType>()
-                .Select(x => new
-                {
-                    Token = x,
-                    Decl = string.Format(fmt, x.ToString())
-                })
-                .ToArray();
-
-            var methodDecls = methods
-                .Select(x => x.Decl)
-                .Aggregate((x, y) => x + "\r\n" + y);
-
-            var expCases = methods
-                .Select(x => string.Format(
-                    "case AphidExpressionType.{0}: Emit{0}(({0})expression); break;",
-                    x.Token))
-                .Aggregate((x, y) => x + "\r\n" + y);
-
-            var expSwitchFmt = @"
-                private void Emit(AphidExpression expression)
-                {{
-                    switch (expression.Type)
-                    {{
-                        {0}
-                        default:
-                            throw new NotImplementedException();
-                    }}
-                }}
-";
-            var expSwitch = string.Format(expSwitchFmt, expCases);
-
-            var code = expSwitch + "\r\n" + methodDecls;
-
-
-            return "";
-        }
     }
 }
