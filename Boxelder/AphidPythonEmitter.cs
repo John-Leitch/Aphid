6b14b1bcfe6a0b351072edb991d6aa899fa0163f
John Leitch <JohnLeitch@outlook.com>
2016-02-28 20:47:05 -0800

    Initial commit of Boxelder.

---
+++ Boxelder/AphidPythonEmitter.cs
@@ -0,0 +1,216 @@
+﻿using Components.Aphid.Compiler;
+using Components.Aphid.Lexer;
+using Components.Aphid.Parser;
+using Components.Aphid.Parser.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Boxelder
+{
+    public class AphidPythonEmitter : AphidStringEmitter
+    {
+        private Dictionary<AphidTokenType, string> _operatorTables = new Dictionary<AphidTokenType, string>
+        {
+            { AphidTokenType.AssignmentOperator, "=" },
+            { AphidTokenType.AdditionOperator, "+" },
+            { AphidTokenType.MinusOperator, "-" },
+            { AphidTokenType.MultiplicationOperator, "*" },
+            { AphidTokenType.DivisionOperator, "/" },
+        };
+
+        private Stack<string> _tabs = new Stack<string>();
+
+        public override string Compile(List<AphidExpression> ast)
+        {
+            var mutators = new AphidMutator[] 
+            {
+                new PipelineToCallMutator(),
+            };
+
+            foreach (var mutator in mutators)
+            {
+                ast = mutator.Mutate(ast);
+            }
+
+            return base.Compile(ast);
+        }
+
+        private void Indent()
+        {
+            _tabs.Push("    ");
+        }
+
+        private void Unindent()
+        {
+            _tabs.Pop();
+        }
+
+        private string GetTabs()
+        {
+            return string.Join("", _tabs);
+        }
+
+        private void AppendTabs()
+        {
+            Append(GetTabs());
+        }
+
+        private void AppendLine()
+        {
+            Append("\r\n");
+        }
+
+        private void AppendStatement(string format, params object[] args)
+        {
+            Append(GetTabs() + format, args);
+        }
+
+        protected override void BeginStatement()
+        {
+            Append(GetTabs());
+        }
+
+        protected override void EndStatement()
+        {
+            AppendLine();
+        }
+
+        protected override void EmitNumberExpression(NumberExpression expression)
+        {
+            Append(expression.Value);
+        }
+
+        protected override void EmitBooleanExpression(BooleanExpression expression)
+        {
+            Append(expression.Value ? "True" : "False");
+        }
+
+        protected override void EmitStringExpression(StringExpression expression)
+        {
+            var escaped = StringParser
+                .Parse(expression.Value)
+                .Replace("\\", "\\\\")
+                .Replace("\n", "\\\n")
+                .Replace("\r", "\\\r")
+                .Replace("'", "\\'");
+
+            Append("'{0}'", escaped);
+        }
+
+        protected override void EmitIdentifierExpression(IdentifierExpression expression)
+        {
+            if (expression.Attributes.Any())
+            {
+                throw new NotImplementedException();
+            }
+
+            Append(expression.Identifier);
+        }
+
+        protected override void EmitUnaryOperatorExpression(UnaryOperatorExpression expression)
+        {
+            switch (expression.Operator)
+            {
+                case AphidTokenType.retKeyword:
+                    Append("return ");
+                    Emit(expression.Operand);
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        protected override void EmitBinaryOperatorExpression(BinaryOperatorExpression expression)
+        {
+            if (expression.RightOperand.Type != AphidExpressionType.FunctionExpression)
+            {
+                Emit(expression.LeftOperand);
+                Append(" {0} ", GetOperator(expression.Operator));
+                Emit(expression.RightOperand);
+            }
+            else
+            {
+                EmitFunctionDecl(expression);
+            }
+        }
+
+        protected void EmitFunctionDecl(BinaryOperatorExpression expression)
+        {
+            var id = expression.LeftOperand.ToIdentifier();
+            var func = expression.RightOperand.ToFunction();
+
+            if (id.Attributes.Any())
+            {
+                throw new NotImplementedException();
+            }
+
+            Append("def {0}(", id.Identifier);
+            EmitTuple(func.Args);
+            Append("):\r\n");
+            Indent();
+            Emit(func.Body);
+            Unindent();
+        }
+
+        protected override void EmitIfExpression(IfExpression expression)
+        {
+            Append("if ");
+            Emit(expression.Condition);
+            Append(":\r\n");
+            Indent();
+            Emit(expression.Body);
+            Unindent();
+
+            if (expression.ElseBody != null && expression.Body.Any())
+            {
+                Append("else:\r\n");
+                Indent();
+                Emit(expression.ElseBody);
+                Unindent();
+            }
+
+            var s = _out.ToString();
+        }
+
+        protected override void EmitCallExpression(CallExpression expression)
+        {
+            Emit(expression.FunctionExpression);
+            Append("(");
+            EmitTuple(expression.Args);
+            Append(")");
+        }
+
+        private void EmitTuple(IEnumerable<AphidExpression> items)
+        {
+            var first = true;
+
+            foreach (var arg in items)
+            {
+                if (!first)
+                {
+                    Append(", ");
+                }
+                else
+                {
+                    first = false;
+                }
+
+                Emit(arg);
+            }
+        }
+
+        private string GetOperator(AphidTokenType op)
+        {
+            string s;
+
+            if (!_operatorTables.TryGetValue(op, out s))
+            {
+                throw new NotImplementedException();
+            }
+
+            return s;
+        }
+    }
+}
---
+++ Boxelder/Program.cs
@@ -0,0 +1,76 @@
+﻿using Components.Aphid.Compiler;
+using Components.Aphid.Lexer;
+using Components.Aphid.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxelder
+{
+    class Program
+    {
+        private const string _code = @"
+x = true;
+func = @(x, y) x + y;
+addTen = @(x) x + 10;
+10 |> addTen |> print;
+print(func(10, 20));
+if (x) print('Hello world');
+else print('Nope I guess');
+";
+
+        static void Main(string[] args)
+        {
+            var emitter = new AphidPythonEmitter();
+            var python = emitter.Compile(AphidParser.Parse(_code));
+            File.WriteAllText(@"C:\temp\test.py", python);
+        }
+
+        private static string GenerateTokenMethods()
+        {
+            var fmt = "protected virtual void Emit{0}({0} expression) {{ " +
+                "throw new NotImplementedException(); }}";
+
+            var methods = Enum
+                .GetValues(typeof(AphidExpressionType))
+                .OfType<AphidExpressionType>()
+                .Select(x => new
+                {
+                    Token = x,
+                    Decl = string.Format(fmt, x.ToString())
+                })
+                .ToArray();
+
+            var methodDecls = methods
+                .Select(x => x.Decl)
+                .Aggregate((x, y) => x + "\r\n" + y);
+
+            var expCases = methods
+                .Select(x => string.Format(
+                    "case AphidExpressionType.{0}: Emit{0}(({0})expression); break;",
+                    x.Token))
+                .Aggregate((x, y) => x + "\r\n" + y);
+
+            var expSwitchFmt = @"
+                private void Emit(AphidExpression expression)
+                {{
+                    switch (expression.Type)
+                    {{
+                        {0}
+                        default:
+                            throw new NotImplementedException();
+                    }}
+                }}
+";
+            var expSwitch = string.Format(expSwitchFmt, expCases);
+
+            var code = expSwitch + "\r\n" + methodDecls;
+
+
+            return "";
+        }
+    }
+}
