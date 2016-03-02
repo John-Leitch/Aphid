8e9898a03579777d64f34b2d0eb28eb0c2529c23
John Leitch <JohnLeitch@outlook.com>
2016-03-01 23:04:39 -0800

    Several Boxelder updates.

---
+++ Boxelder/ConsolePlus/Cli.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Components.ConsolePlus
+{
+    public static class Cli
+    {
+        private const string _messageFormat = "[~{0}~{1}~R~] {2}";
+
+        public const char QueryChar = '?',
+          InfoChar = 'i',
+          SuccessChar = '+',
+          StringChar = 's',
+          BinaryChar = 'b',
+          ErrorChar = '-',
+          FatalErrorChar = 'x';
+
+        private const string nameHeader = "Name";
+
+        private const string valueHeader = "Value";
+
+        private static int maxNameLength;
+
+        private static int maxValueLength;
+
+        private static object _syncObject = new object();
+
+        private static bool _useTrace = false;
+
+        public static bool UseTrace
+        {
+            get { return Cli._useTrace; }
+            set
+            {
+                Cli._useTrace = value;
+
+                if (value)
+                {
+                    _write = x => Trace.Write(x);
+                    _writeLine = x => Trace.WriteLine(x);
+                }
+                else
+                {
+                    _write = Console.Write;
+                    _writeLine = Console.WriteLine;
+                }
+            }
+        }
+
+        private static Action<string> _write = Console.Write;
+
+        private static Action<string> _writeLine = Console.WriteLine;
+
+        static Cli()
+        {
+            try
+            {
+                SetLengths(Console.BufferWidth);
+            }
+            catch (IOException)
+            {
+                SetLengths(100);
+            }
+        }
+
+        private static void SetLengths(int consoleWidth)
+        {
+            maxNameLength = (int)Math.Floor(((double)consoleWidth - 3) / 2);
+            maxValueLength = (int)Math.Ceiling(((double)consoleWidth - 4) / 2);
+        }
+
+        /// <summary>
+        /// Writes a format string to the console.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg">The arguments.</param>
+        [DebuggerStepThrough]
+        public static void Write(string format, params object[] arg)
+        {
+            WriteCore(format, false, arg);
+        }
+
+        /// <summary>
+        /// Writes a format string and a new line to the console.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg">The arguments.</param>
+        [DebuggerStepThrough]
+        public static void WriteLine(string format, params object[] arg)
+        {
+            WriteCore(format, true, arg);
+        }
+
+        [DebuggerStepThrough]
+        public static void WriteLine()
+        {
+            _writeLine("");
+        }        
+
+        public static void DumpTable(IEnumerable<KeyValuePair<string, string>> nameValuePairs)
+        {
+            if (nameValuePairs.Count() == 0)
+            {
+                return;
+            }
+
+            const int paddingSize = 0;
+            var longestNameLength = nameValuePairs.OrderByDescending(x => x.Key.Length).First().Key.Length;
+
+            if (longestNameLength < nameHeader.Length)
+            {
+                longestNameLength = nameHeader.Length;
+            }
+            else if (longestNameLength > maxNameLength)
+            {
+                longestNameLength = maxNameLength;
+            }
+
+            var longestValueLength = nameValuePairs.OrderByDescending(x => x.Value.Length).First().Value.Length;
+
+            if (longestValueLength < valueHeader.Length)
+            {
+                longestValueLength = valueHeader.Length;
+            }
+            else if (longestValueLength > maxValueLength)
+            {
+                longestValueLength = maxValueLength;
+
+                if (longestNameLength < maxNameLength)
+                {
+                    longestValueLength += maxNameLength - longestNameLength;
+                }
+            }
+
+            var padding = new string(' ', paddingSize);
+
+            var headerColor = "~White~~|DarkGray~";
+
+            var totalLength = paddingSize * 4 + 3 + longestNameLength + longestValueLength;
+
+            //var newLine = totalLength == Console.BufferWidth ? "" : "\r\n";
+            var newLine = "\r\n";
+
+            Func<string, string, bool, string> createRow = (name, value, header) =>
+            {
+                if (name.Length > maxNameLength)
+                {
+                    name = name.Remove(maxNameLength - 3) + "~Yellow~...~R~";
+                }
+
+                if (value.Length > longestValueLength)
+                {
+                    value = value.Remove(longestValueLength - 3) + "~Yellow~...~R~";
+                }
+
+                var delim = "║" + (header ? headerColor : "") + padding;
+                var reset = (header ? "~R~" : "");
+                return delim + name.PadRight(longestNameLength + paddingSize) + reset +
+                    delim + value.PadRight(longestValueLength + paddingSize) + reset + "║";
+            };
+
+            var divider = new string('═', totalLength - 2).ToCharArray();
+            var verticalRuleOffset = longestNameLength + paddingSize * 2;
+            divider[verticalRuleOffset] = '╦';
+            var hrTop = "╔" + new string(divider) + "╗";
+            divider[verticalRuleOffset] = '╬';
+            var hrMiddle = "╠" + new string(divider) + "╣";
+            divider[verticalRuleOffset] = '╩';
+            var hrBottom = "╚" + new string(divider) + "╝";
+
+            var rows = nameValuePairs
+                .Select(x => createRow(x.Key, x.Value, false))
+                .Aggregate((x, y) => x + newLine + y);
+
+            WriteLine(hrTop + newLine +
+                createRow(nameHeader, valueHeader, true) + newLine +
+                hrMiddle + newLine +
+                rows + newLine +
+                hrBottom);
+        }
+
+        private static void DumpDetails(IEnumerable<KeyValuePair<string, string>> nameValuePairs)
+        {
+            foreach (var nvp in nameValuePairs)
+            {
+                WriteLine(
+                    "~{0}~~|{1}~{2}~R~\r\n{3}\r\n",
+                    ConsoleColor.White,
+                    ConsoleColor.Blue,
+                    nvp.Key,
+                    nvp.Value);
+            }
+        }
+
+        public static IEnumerable<KeyValuePair<string, string>> CreateTable(object obj, bool escapeLines)
+        {
+            Func<PropertyInfo, string> tryGet = x =>
+            {
+                try
+                {
+#if NET35
+                    return (x.GetValue(obj, null) ?? "null").ToString();
+#else
+                    return (x.GetValue(obj) ?? "null").ToString();
+#endif
+                }
+                catch (Exception e)
+                {
+                    return string.Format("Error getting value: {0}", e);
+                }
+            };
+            var t = obj.GetType();
+            return t
+                .GetProperties()
+                .Select(x => new KeyValuePair<string, string>(
+                    x.Name,
+                    escapeLines ?
+                        tryGet(x).Replace("\r", "\\r").Replace("\n", "\\n") :
+                        tryGet(x)
+                ));
+        }
+
+        public static void Dump<T>(T obj, DumpMode mode)
+        {
+            var nvps = CreateTable(obj, mode == DumpMode.Table);
+
+            if (mode == DumpMode.Table)
+            {
+                DumpTable(nvps);
+            }
+            else if (mode == DumpMode.Details)
+            {
+                DumpDetails(nvps);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid mode.");
+            }
+        }
+
+        public static void Dump<T>(T obj)
+        {
+            Dump(obj, DumpMode.Table);
+        }
+
+#if NET35
+        public static void WriteHeader(string format)
+        {
+            WriteHeader(format, "");
+        }
+
+        public static void WriteHeader(string format, string style)
+#else
+        public static void WriteHeader(string text, string style = "")
+#endif
+        {
+            var divider = new string('═', Console.BufferWidth - 3).ToCharArray();
+            var hrTop = "╔" + new string(divider) + "╗\r\n";
+            var hrMiddle = "║" + style + " " + text.PadRight(Console.BufferWidth - 4).Replace("~", "~~") + "~R~║\r\n";
+            var hrBottom = "╚" + new string(divider) + "╝\r\n";
+            Write(hrTop + hrMiddle + hrBottom);
+        }
+
+        public static void WriteSubheader(string text, string style = "")
+        {
+            Cli.WriteLine(
+                "{0}  {1}{2}{3}",
+                style,
+                Cli.Escape(text),
+                new string(' ', Console.BufferWidth - text.Length - 3),
+                "~R~");
+        }
+
+        /// <summary>
+        /// The core console write function.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="newLine">Determines whether to print the message with a new line or not.</param>
+        /// <param name="arg">The arguments.</param>
+        [DebuggerStepThrough]
+        private static void WriteCore(string format, bool newLine, params object[] arg)
+        {
+            lock (_syncObject)
+            {
+                format = string.Format(format, arg);
+                var state = CliLexerState.ReadingText;
+
+                var buffer = new StringBuilder();
+
+                for (int i = 0; i < format.Length; i++)
+                {
+                    var c = format[i];
+
+                    if (state == CliLexerState.ReadingText)
+                    {
+                        if (c == '~')
+                        {
+                            state = CliLexerState.ReadingToken;
+
+                            if (buffer.Length != 0)
+                            {
+                                _write(buffer.ToString());
+
+#if NET35
+                                buffer = new StringBuilder();
+#else
+                                buffer.Clear();
+#endif
+                            }
+
+                            continue;
+                        }
+                    }
+                    else if (state == CliLexerState.ReadingToken)
+                    {
+                        if (c == '~')
+                        {
+                            if (buffer.Length == 0)
+                            {
+                                buffer.Append('~');
+                                state = CliLexerState.ReadingText;
+                                continue;
+                            }
+                            else
+                            {
+                                var token = buffer.ToString();
+
+                                #if NET35
+                                buffer = new StringBuilder();
+#else
+                                buffer.Clear();
+#endif
+
+                                if (token == "R")
+                                {
+                                    Console.ResetColor();
+                                }
+                                else
+                                {
+                                    var isBackgroundColor = token[0] == '|';
+
+                                    var color = (ConsoleColor)Enum.Parse(
+                                        typeof(ConsoleColor),
+                                        isBackgroundColor ? token.Substring(1) : token);
+
+                                    if (isBackgroundColor)
+                                    {
+                                        Console.BackgroundColor = color;
+                                    }
+                                    else
+                                    {
+                                        Console.ForegroundColor = color;
+                                    }
+                                }
+
+                                state = CliLexerState.ReadingText;
+                                continue;
+                            }
+                        }
+                    }
+
+                    buffer.Append(c);
+                }
+
+                if (state == CliLexerState.ReadingToken)
+                {
+                    throw new ArgumentException("Invalid token in format string");
+                }
+
+                if (newLine)
+                {
+                    _writeLine(buffer.ToString());
+                }
+                else
+                {
+                    _write(buffer.ToString());
+                }
+            }
+        }
+
+        public static string Escape(string value)
+        {
+            return !string.IsNullOrEmpty(value) ? 
+                value
+                    .Replace("{", "{{")
+                    .Replace("}", "}}")
+                    .Replace("~", "~~") : 
+                null;
+        }
+
+        public static void WriteMessage(ConsoleColor tokenColor, char token, string format, params object[] arg)
+        {
+            Cli.WriteLine(
+                string.Format(_messageFormat, tokenColor, token, format),
+                arg);
+        }
+
+        public static void WriteInfoMessage(string format, params object[] arg)
+        {
+            WriteMessage(
+                ConsoleColor.White,
+                InfoChar,
+                format,
+                arg);
+        }
+
+        public static void WriteQueryMessage(string format, params object[] arg)
+        {
+            WriteMessage(
+                ConsoleColor.White,
+                QueryChar,
+                format,
+                arg);
+        }
+
+        public static void WriteSuccessMessage(string format, params object[] arg)
+        {
+            WriteMessage(
+                ConsoleColor.Green,
+                SuccessChar,
+                format,
+                arg);
+        }
+
+        public static void WriteErrorMessage(string format, params object[] arg)
+        {
+            WriteMessage(
+                ConsoleColor.Red,
+                ErrorChar,
+                format,
+                arg);
+        }
+
+        public static void WriteCriticalErrorMessage(string format, params object[] arg)
+        {
+            WriteMessage(
+                ConsoleColor.Red,
+                FatalErrorChar,
+                format,
+                arg);
+        }
+    }
+}
--- Boxelder/Program.cs
+++ Boxelder/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Components.Aphid.Compiler;
 using Components.Aphid.Lexer;
 using Components.Aphid.Parser;
+using Components.ConsolePlus;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,19 +14,98 @@ namespace Boxelder
 {
     class Program
     {
-        private const string _code = @"
-class foo { x: 20, y: 30 };
-obj = foo();
-add = @(x, y) x + y;
-obj.x |> @add(10) |> print;
-";
-
-        static void Main(string[] args)
+        private static void ValidateArgs(string[] args)
         {
-            var ast = AphidParser.Parse(_code);
-            var emitter = new AphidPythonEmitter();
-            var python = emitter.Compile(ast);
-            File.WriteAllText(@"C:\temp\test.py", python);
+            if (args.Length == 0)
+            {
+                WriteDirections();
+                Environment.Exit(1);
+            }
+            else if (args.Length > 1)
+            {
+                Cli.WriteLine("Invalid arguments.");
+                Environment.Exit(2);
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Cli.WriteLine("Could not find input file '~Yellow~{0}~R~'.", args[0]);
+                Environment.Exit(3);
+            }
+            
+        }
+
+        private static void Main(string[] args)
+        {
+            WriteHeader();
+            ValidateArgs(args);
+            Cli.WriteInfoMessage("Parsing '~Cyan~{0}~R~'", args[0]);
+            var ast = ParseCode(args[0]);
+            Cli.WriteSuccessMessage("File successfully parsed");
+            var outFile = GetOutFilename(args);
+            EmitCode(ast, outFile);
+        }
+
+        private static string GetOutFilename(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 1:
+                    return Path.GetFileNameWithoutExtension(args[0]) + ".py";
+                case 2:
+                    return args[1];
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private static List<AphidExpression> ParseCode(string filename)
+        {
+            var code = File.ReadAllText(filename);
+
+            try
+            {
+                return AphidParser.Parse(code);
+            }
+            catch (AphidParserException exception)
+            {
+                var msg = ParserErrorMessage.Create(code, exception);
+                Cli.WriteLine(Cli.Escape(msg));
+                Environment.Exit(4);
+                throw;
+            }
+        }
+
+        private static void EmitCode(List<AphidExpression> ast, string filename)
+        {
+            try
+            {
+                var emitter = new AphidPythonEmitter();
+                Cli.WriteInfoMessage("Compiling to Python");
+                var python = emitter.Compile(ast);
+                File.WriteAllText(filename, python);
+                Cli.WriteSuccessMessage("Compilation successful, Python written to '~Cyan~{0}~R~'", filename);
+            }
+            catch (Exception e)
+            {
+                Cli.WriteCriticalErrorMessage("Compilation error: ~Yellow~{0}~R~", e.Message);
+                Environment.Exit(5);
+            }
+        }
+
+        private static void WriteHeader()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            var header = string.Format("Boxelder {0}", version);
+            Cli.WriteHeader(header, "~|Blue~~White~");
+            Cli.WriteLine();
+        }
+
+        private static void WriteDirections()
+        {
+            Cli.WriteLine("Compiles an Aphid program (~Cyan~*.alx~R~) program into Python (~Cyan~*.py~R~).\r\n");
+            Cli.WriteLine("be ~|DarkGray~~White~script file~R~\r\n");
+            Cli.WriteLine("e.g. be ~Cyan~HelloWorld.alx~R~");
         }
     }
 }
