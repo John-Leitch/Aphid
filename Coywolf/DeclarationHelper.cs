228b31ce7804a40b3618afb9d35fa5592d392313
John Leitch <JohnLeitch@outlook.com>
2016-03-06 18:15:41 -0800

    Added DeclarationHelper.

---
+++ Coywolf/DeclarationHelper.cs
@@ -0,0 +1,43 @@
+﻿using Components.Aphid.Lexer;
+using Components.Aphid.Parser;
+using Components.Aphid.Parser.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coywolf
+{
+    public static class DeclarationHelper
+    {
+        public static IdentifierExpression GetDeclaredIdentifier(AphidExpression expression)
+        {
+            switch (expression.Type)
+            {
+                case AphidExpressionType.ForEachExpression:
+                    var element = expression.ToForEach().Element;
+
+                    if (element.Type == AphidExpressionType.IdentifierExpression)
+                    {
+                        return element.ToIdentifier();
+                    }
+
+                    break;
+
+                case AphidExpressionType.BinaryOperatorExpression:
+                    var binOpExp = expression.ToBinaryOperator();
+
+                    if (binOpExp.Operator == AphidTokenType.AssignmentOperator &&
+                        binOpExp.LeftOperand.Type == AphidExpressionType.IdentifierExpression)
+                    {
+                        return binOpExp.LeftOperand.ToIdentifier();
+                    }
+
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
