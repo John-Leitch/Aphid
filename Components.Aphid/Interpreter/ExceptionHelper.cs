d463b8e8cef2d62516976997581bb4f78d6478b6
John Leitch <JohnLeitch@outlook.com>
2016-03-11 23:01:09 -0800

    Added ExceptionHelper.

---
+++ Components.Aphid/Interpreter/ExceptionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Aphid.Interpreter
+{
+    public static class ExceptionHelper
+    {
+        public static Exception Unwrap(Exception exception)
+        {
+            while (true)
+            {
+                var ex = exception as TargetInvocationException;
+                
+                if (ex != null)
+                {
+                    exception = ex.InnerException;
+                }
+                else
+                {
+                    return exception;
+                }
+            }
+        }
+    }
+}
