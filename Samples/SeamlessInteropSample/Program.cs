2465865f5b5e2de62a93ce1d1a470b8e3ec9913b
John Leitch <JohnLeitch@outlook.com>
2016-04-12 18:43:56 -0700

    Added seamless interop sample.

---
+++ Samples/SeamlessInteropSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Components.Aphid.Interpreter;
+
+namespace SeamlessInteropSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interpreter = new AphidInterpreter();
+            
+            interpreter.Interpret(@"
+                using System;
+                Console.WriteLine('Hello world');
+            ");
+        }
+    }
+}
