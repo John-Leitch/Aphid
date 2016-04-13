8c0289d3cd11185adab32c9ea2971a8b3e2dcba3
John Leitch <JohnLeitch@outlook.com>
2016-04-12 19:37:03 -0700

    Added more seamless interop samples.

---
+++ Samples/SeamlessInteropSample2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Components.Aphid.Interpreter;
+
+namespace SeamlessInteropSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interpreter = new AphidInterpreter();
+
+            interpreter.Interpret(@"
+                using System;
+                using System.Text;
+                sb = new StringBuilder('Hello');
+                sb.Append(' world');
+                
+                Console.WriteLine(
+                    'Length={0}, Capacity={1}', 
+                    sb.Length,
+                    sb.Capacity);
+
+                sb |> Console.WriteLine;
+            ");
+        }
+    }
+}
---
+++ Samples/SeamlessInteropSample3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Components.Aphid.Interpreter;
+
+namespace SeamlessInteropSample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interpreter = new AphidInterpreter();
+
+            interpreter.Interpret(@"
+                load System.Web.Extensions;
+                using System;
+                using System.Web.Script.Serialization;
+                serializer = new JavaScriptSerializer();
+                
+                obj = 
+                    '{ ""x"":52, ""y"":30 }' 
+                    |> serializer.DeserializeObject;
+                
+                Console.WriteLine(
+                    'x={0}, y={1}',
+                    obj.get_Item('x'),
+                    obj.get_Item('y'));
+            ");
+        }
+    }
+}
