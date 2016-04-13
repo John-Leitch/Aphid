2d36fd692204a2054849aea608ac6ab4ac09b85d
John Leitch <JohnLeitch@outlook.com>
2016-04-12 17:14:28 -0700

    Added MacroSample project.

---
+++ Samples/MacroSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Components.Aphid.Interpreter;
+
+namespace MacroSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interpreter = new AphidInterpreter();
+
+            interpreter.Interpret(@"
+                #'std';
+                m1 = macro(@{ 'Hello world' });
+                m2 = macro(@(msg) { print(msg) });
+                
+                m3 = macro(@{
+                    m2('foobar');
+                    m2(m1());
+                });
+                
+                m3();
+            ");
+        }
+    }
+}
