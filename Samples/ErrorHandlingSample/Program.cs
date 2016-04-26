33e243daa363a6b9ab219213762e19538c1551cb
John Leitch <JohnLeitch@outlook.com>
2016-04-25 22:33:18 -0700

    Added ErrorHandlingSample project.

---
+++ Samples/ErrorHandlingSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using Components.Aphid.Interpreter;
+using Components.Aphid.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandlingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = @"
+                #'std';
+                print('foo'bar');
+            ";
+
+            try
+            {
+                var interpreter = new AphidInterpreter();
+                interpreter.Interpret(code);
+            }
+            catch (AphidParserException e)
+            {
+                var msg = ParserErrorMessage.Create(code, e);
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
