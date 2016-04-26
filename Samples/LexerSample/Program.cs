73dc33a7d3a4827d914e7230c9f32c508e103e2d
John Leitch <JohnLeitch@outlook.com>
2016-04-25 20:33:00 -0700

    Added LexerSample.

---
+++ Samples/LexerSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using Components.Aphid.Lexer;
+using System;
+
+namespace LexerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lexer = new AphidLexer(@"
+                using System;
+                Console.WriteLine('Hello world');
+            ");
+
+            foreach (var t in lexer.GetTokens())
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
