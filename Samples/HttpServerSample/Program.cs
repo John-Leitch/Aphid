ccabf40da4955cfd20c834868cfffec68a8361fc
John Leitch <JohnLeitch@outlook.com>
2016-05-05 22:34:57 -0700

    Added HTTP server sample.

---
+++ Samples/HttpServerSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Components.Aphid.Library;
+using Components.External;
+using System.Threading;
+
+namespace HttpServerSample
+{
+    class Program
+    {
+        private const string _prefix = "http://localhost:8888/";
+
+        static void Main(string[] args)
+        {
+            var webRoot = PathHelper.GetExecutingPath("WebRoot");
+            var server = new HttpServer(_prefix, webRoot);
+            server.Start();
+            Thread.Sleep(-1);
+        }
+    }
+}
