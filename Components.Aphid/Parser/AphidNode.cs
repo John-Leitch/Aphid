7f6ceb4f866842f6d91c5ad0ea710144b456c96f
John Leitch <JohnLeitch@outlook.com>
2016-05-06 00:55:19 -0700

    Added AphidNode.IsStatement().

--- Components.Aphid/Parser/AphidNode.cs
+++ Components.Aphid/Parser/AphidNode.cs
@@ -4,7 +4,27 @@ namespace Components.Aphid.Parser
 {
     public abstract class AphidNode
     {
-        public abstract AphidExpressionType Type { get; }        
+        public abstract AphidExpressionType Type { get; }
+
+        public bool IsStatement()
+        {
+            switch (Type)
+            {
+                case AphidExpressionType.IfExpression:
+                case AphidExpressionType.ForExpression:
+                case AphidExpressionType.ForEachExpression:
+                case AphidExpressionType.WhileExpression:
+                case AphidExpressionType.DoWhileExpression:
+                case AphidExpressionType.ExtendExpression:
+                case AphidExpressionType.TryExpression:
+                case AphidExpressionType.SwitchExpression:
+                    return true;
+
+                default:
+                    return false;
+            }
+                
+        }
     }
 }
 
