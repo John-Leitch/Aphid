52b337d0fd10dd0c77d02005117d5d3b43f62805
John Leitch <JohnLeitch@outlook.com>
2017-12-18 21:33:04 -0800

    Updated serializer tests.

--- Components.Aphid.Tests.Integration/SerializerTests.cs
+++ Components.Aphid.Tests.Integration/SerializerTests.cs
@@ -19,67 +19,122 @@ namespace Components.Aphid.Tests.Integration
         private void AssertSerialization(object expected, string objectDecl, string code)
         {
             var script = string.Format(
-                "o = {0} |> serialize |> deserialize;\r\n{1}",
+                "o = ({0}) |> serialize |> deserialize;\r\n{1}",
                 objectDecl,
                 code);
 
             AssertEquals(expected, script);
         }
 
-        private void AssertFooSerialization(string objectDecl, string code)
+        private void AssertFooDeserialization(string objectDecl, string code)
         {
             AssertSerialization("foo", objectDecl, code);
         }
 
-        private void Assert9Serialization(string objectDecl, string code)
+        private void Assert9Deserialization(string objectDecl, string code)
         {
             AssertSerialization(9, objectDecl, code);
         }
 
-        [Test]
-        public void SerializeStringTest()
+        private void AssertTrueDeserialization(string objectDecl, string code)
         {
-            AssertFooSerialization("'foo'", "ret o");
+            AssertSerialization(true, objectDecl, code);
+        }
+
+        private void AssertFalseDeserialization(string objectDecl, string code)
+        {
+            AssertSerialization(false, objectDecl, code);
         }
 
         [Test]
-        public void SerializeStringTest2()
+        public void TestStringSerialization()
         {
-            AssertFooSerialization("{ x: 'foo' }", "ret o.x");
+            AssertFooDeserialization("'foo'", "ret o");
         }
 
         [Test]
-        public void SerializeStringTest3()
+        public void TestStringSerialization2()
         {
-            AssertFooSerialization("{ vec: { x: 'foo' } }", "ret o.vec.x");
+            AssertFooDeserialization("{ x: 'foo' }", "ret o.x");
+        }
+
+        [Test]
+        public void TestStringSerialization3()
+        {
+            AssertFooDeserialization("{ vec: { x: 'foo' } }", "ret o.vec.x");
         }
 
         /// <summary>
         /// Todo: fix AphidObjectLexer.
         /// </summary>
         [Test]
-        public void SerializeNumberTest()
+        public void TestNumberSerialization()
         {
-            Assert9Serialization(
+            Assert9Deserialization(
                 "@{ using Components.Aphid.Interpreter; ret ValueHelper.Wrap(9) }()",
                 "ret o");
         }
 
         [Test]
-        public void SerializeNumberTest2()
+        public void TestNumberSerialization2()
         {
-            Assert9Serialization("{ x: 9 }", "ret o.x");
+            Assert9Deserialization("{ x: 9 }", "ret o.x");
         }
 
         [Test]
-        public void SerializeNumberTest3()
+        public void TestNumberSerialization3()
         {
-            Assert9Serialization("{ vec: { x: 9 } }", "ret o.vec.x");
+            Assert9Deserialization("{ vec: { x: 9 } }", "ret o.vec.x");
         }
 
-        //[Test]
-        //public void ConstantFoldingTest2()
-        //{
-        //}
+        [Test]
+        public void TestListSerialization()
+        {
+            AssertFooDeserialization("[ 'foo' ]", "ret o[0]");
+        }
+
+        [Test]
+        public void TestListSerialization2()
+        {
+            AssertFooDeserialization("[ null, 'foo' ]", "ret o[1]");
+        }
+
+        [Test]
+        public void TestListSerialization3()
+        {
+            Assert9Deserialization("[ 9 ]", "ret o[0]");
+        }
+
+        [Test]
+        public void TestListSerialization4()
+        {
+            Assert9Deserialization("[ null, 9 ]", "ret o[1]");
+        }
+
+        [Test]
+        public void TestListSerialization5()
+        {
+            Assert9Deserialization("0..9", "ret o.Count");
+        }
+
+        [Test]
+        public void TestListSerialization6()
+        {
+            AssertTrueDeserialization("0..0", "ret o.Count == 0");
+        }
+
+        [Test]
+        public void TestListSerialization7()
+        {
+            AssertTrueDeserialization("0..0x1000", "ret o.Count == 0x1000");
+        }
+
+        [Test, Ignore]
+        public void TestListSerialization8()
+        {
+            Assert9Deserialization(
+                "(0..0x100)->@() $_ * 2",
+                "ret o.Count == 0x100 && o[0x2] == 0x4");
+        }
     }
 }
