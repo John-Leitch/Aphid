550662bbc5a97dc6d3e6529c5c54b5be29b989d5
John-Leitch <JohnLeitch@outlook.com>
2018-03-10 17:41:59 -0500

    Added several new pattern matching tests.

--- Components.Aphid.Tests.Integration/PatternMatchingTests.cs
+++ Components.Aphid.Tests.Integration/PatternMatchingTests.cs
@@ -28,5 +28,53 @@ namespace Components.Aphid.Tests.Integration
         {
             Assert9("x = 'foo'; ret $(x) 'foo':9, 'bar':1, 0;");
         }
+
+        [Test]
+        public void PatternMatchingIdentifierCaseTest()
+        {
+            Assert9("f = 'foo'; x = 'foo'; ret $(x) 'bar':1, f:9, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingDecimalIntTest()
+        {
+            Assert9("using System; x = 10; ret $(x) Convert.ToInt32(10): 9, 20: 1, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingIntDecimalTest()
+        {
+            Assert9("using System; x = Convert.ToInt32(10); ret $(x) 10: 9, 20: 1, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingDecimalUnsignedIntTest()
+        {
+            Assert9("using System; x = 10; ret $(x) Convert.ToUInt32(10): 9, 20: 1, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingUnsignedIntDecimalTest()
+        {
+            Assert9("using System; x = Convert.ToUInt32(10); ret $(x) 10: 9, 20: 1, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingNullCaseTest()
+        {
+            Assert9("x = 'foo'; ret $(x) null: 0, 'foo':9, 'bar':1, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingNullTest()
+        {
+            Assert9("x = null; ret $(x) 10: 1, null: 9, 0;");
+        }
+
+        [Test]
+        public void PatternMatchingNullDefaultTest()
+        {
+            Assert9("x = null; ret $(x) 10: 1, 2: 0, 9;");
+        }
     }
 }
