d01d4830bb5ff6f48907b8fa795e2e69431c8114
John-Leitch <JohnLeitch@outlook.com>
2018-03-25 15:34:30 -0400

    Added static AphidLexer helper methods.

--- Components.Aphid/Lexer/AphidLexer.cs
+++ Components.Aphid/Lexer/AphidLexer.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace Components.Aphid.Lexer
 {
+    // Todo: generate based on Mantispin input
     public partial class AphidLexer
     {
         public void SetCodeMode()
@@ -20,6 +21,45 @@ namespace Components.Aphid.Lexer
         public void SetShellMode()
         {
             mode = 2;
+        }
+
+        public static List<AphidToken> GetTokens(string text)
+        {
+            return new AphidLexer(text).GetTokens();
+        }
+
+        public static List<AphidToken> GetAllTokens(string text)
+        {
+            return new AphidLexer(text).GetAllTokens();
+        }
+
+        public static AphidToken GetToken(string text)
+        {
+            return GetToken(text, 0);
+        }
+
+        public static AphidToken GetToken(string text, int offset)
+        {
+            var lexer = new AphidLexer(text);
+            lexer.charIndex = offset - 1;
+            var type = lexer.GetToken();
+            var index = lexer.lastIndex + 1;
+            var lexeme = text.Substring(index, lexer.charIndex - lexer.lastIndex);
+
+            return new AphidToken(type, lexeme, index);
+        }
+
+        public static AphidTokenType GetTokenType(string text)
+        {
+            return GetTokenType(text, 0);
+        }
+
+        public static AphidTokenType GetTokenType(string text, int offset)
+        {
+            var lexer = new AphidLexer(text);
+            lexer.charIndex = offset - 1;
+
+            return lexer.GetToken();
         }
     }
 }
