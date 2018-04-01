738d20917a229deffd57f4d95086f7b144f3e153
John-Leitch <JohnLeitch@outlook.com>
2018-04-01 10:06:48 -0400

    Prototyped template for generating AST node Equals() and GetHashCode().

--- Components.Aphid/Parser/IdentifierExpression.cs
+++ Components.Aphid/Parser/IdentifierExpression.cs
@@ -11,5 +11,46 @@ namespace Components.Aphid.Parser
         {
             return Code == null ? Identifier : base.ToString();
         }
+
+        // Todo: use Equals and GetHashCode overrides as template for Mantispid.
+        public override bool Equals(object obj)
+        {
+            var other = obj as IdentifierExpression;
+
+            return other != null &&
+                Identifier == other.Identifier &&
+                ((Attributes == null && other.Attributes == null) ||
+                (Attributes.SequenceEqual(other.Attributes)));
+        }
+
+        public override int GetHashCode()
+        {
+            const int seed1 = 0x40000000,
+                seed2 = seed1 >> 1,
+                seed3 = seed2 >> 1,
+                seed4 = seed3 >> 1;
+
+            unchecked
+            {
+                var hash = Identifier != null ? Identifier.GetHashCode() : seed1;
+
+                if (hash == 0)
+                {
+                    hash = seed2;
+                }
+
+                var tmp = Attributes != null ? Attributes.GetHashCode() : seed3;
+
+                if (tmp == 0)
+                {
+                    tmp = seed4;
+                }
+
+                hash *= tmp;
+
+
+                return hash;
+            }
+        }
     }
 }
