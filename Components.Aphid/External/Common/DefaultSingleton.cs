2f1560a92bf1fa7ddc8755e92e722d82cc612f6d
John-Leitch <JohnLeitch@outlook.com>
2018-04-22 01:46:06 -0400

    Added DefaultSingleton class.

---
+++ Components.Aphid/External/Common/DefaultSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    public abstract class DefaultSingleton<T>
+            where T : DefaultSingleton<T>, new()
+    {
+        public static T Current { get; private set; }
+
+        public DefaultSingleton()
+        {
+            if (Current != null)
+            {
+                throw new InvalidOperationException(
+                    string.Format("Can only create one instance of '{0}'.", typeof(T)));
+            }
+
+            Current = (T)this;
+        }
+
+        static DefaultSingleton()
+        {
+            if (Current == null)
+            {
+                new T();
+            }
+        }
+    }
+}
