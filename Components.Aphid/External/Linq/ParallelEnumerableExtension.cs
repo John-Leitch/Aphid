9ceb2b06ce98fa1f9061d39227594947c3891893
John Leitch <JohnLeitch@outlook.com>
2018-12-06 08:04:17 -0500

    Added ParallelEnumerableExtension.

---
+++ Components.Aphid/External/Linq/ParallelEnumerableExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+
+namespace Components
+{
+    public static class ParallelEnumerableExtension
+    {
+        public static ParallelQuery<TSource> ForceUnbufferedPerProcessorParallelism<TSource>(
+            this IEnumerable<TSource> source,
+            int processorCountMultiplier,
+            int minDegreesOfParallelism) =>
+            source
+                .AsParallel()
+                .WithDegreeOfParallelism(
+                    Math.Max(
+                        Environment.ProcessorCount * processorCountMultiplier,
+                        minDegreesOfParallelism))
+                .WithMergeOptions(ParallelMergeOptions.NotBuffered)
+                .WithExecutionMode(ParallelExecutionMode.ForceParallelism);
+
+        public static ParallelQuery<TSource> ForceUnbufferedPerProcessorParallelism<TSource>(
+            this IEnumerable<TSource> source,
+            int processorCountMultiplier) =>
+            source.ForceUnbufferedPerProcessorParallelism(processorCountMultiplier, 4);
+
+        public static ParallelQuery<TSource> ForceUnbufferedPerProcessorParallelism<TSource>(
+            this IEnumerable<TSource> source) =>
+            source.ForceUnbufferedPerProcessorParallelism(1);
+    }
+}
