127101551ba544bba52a1e62cb695147a29ecc8c
John Leitch <JohnLeitch@outlook.com>
2019-05-27 02:17:43 -0400

    Fixed hwnd dock issues.

---
+++ AphidUI/Components/HwndDockControl.cs
@@ -0,0 +1,398 @@
+﻿using Components.PInvoke;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using static Components.PInvoke.GWL;
+using static Components.PInvoke.NativeWindowStyle;
+using static Components.PInvoke.User32;
+using static Components.PInvoke.Win32;
+using static System.Math;
+using WinFlag = Components.PInvoke.SetWindowPosFlags;
+using WinStyle = Components.PInvoke.NativeWindowStyle;
+
+namespace Components.Aphid.Wpf
+{
+    public partial class HwndDockControl : UserControl
+    {
+        public static readonly RoutedEvent DockedWindowClosedEvent =
+            EventManager.RegisterRoutedEvent(
+                nameof(Delegate ),
+                RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler),
+                typeof(HwndDockControl));
+
+        public event RoutedEventHandler Trye
+        {
+            add { AddHandler(DockedWindowClosedEvent, value); }
+            remove { RemoveHandler(DockedWindowClosedEvent, value); }
+        }
+
+        public static readonly DependencyProperty DockedProcessProperty =
+            DependencyProperty.Register(
+            nameof(DockedProcess),
+            typeof(Process),
+            typeof(HwndDockControl),
+            new PropertyMetadata(null, DockedProcessPropertyChanged));
+
+        public bool DockedProcess
+        {
+            get => (bool)GetValue(DockedProcessProperty);
+            set => SetValue(DockedProcessProperty, value);
+        }
+
+        public IntPtr OriginalParentThread { get; set; }
+
+        public WinStyle[] OriginalStyles { get; set; }
+
+        private bool _isLoaded = false, _isChildVisible = false;
+
+        private IntPtr _childHwnd, _parentHwnd, _originalParentHwnd;
+
+        private Int32Rect _childPos = new Int32Rect();        
+
+        public Window ParentWindow { get; private set; }
+
+        public FrameworkElement DockingTarget { get; private set; }
+
+        public bool RemoveDockedWindowBorders { get; set; } = true;
+
+        public string FileName { get; set; }
+
+        public string Arguments { get; set; }
+
+        public string FindWindow { get; set; }
+
+        public int? ProcessId { get; set; }
+
+        public bool HasDockedWindow => _childHwnd != IntPtr.Zero;
+
+        public HwndDockControl()
+        {
+            DockingTarget = this;
+            DockingTarget.SizeChanged += (o, e) => DrawWindow();
+            DockingTarget.IsVisibleChanged += (o, e) => SetWindowVisibility((bool)e.NewValue);
+
+            Initialized += (o, e) => ParentWindow = Window.GetWindow(this);
+            Loaded += HwndDockControlLoaded;
+        }
+
+        private void HwndDockControlLoaded(object sender, RoutedEventArgs e)
+        {
+            if (_isLoaded)
+            {
+                return;
+            }
+
+            _isLoaded = true;
+
+            if (ParentWindow == null)
+            {
+                ParentWindow = Window.GetWindow(this);
+            }
+
+            if (!string.IsNullOrEmpty(FileName))
+            {
+                StartProcess(FileName, Arguments);
+            }
+            else if (!string.IsNullOrEmpty(FindWindow))
+            {
+                FindByClassName(FindWindow);
+            }
+            else if (ProcessId != null)
+            {
+                var handle = Process.GetProcessById((int)ProcessId).MainWindowHandle;
+                Attach(handle);
+            }
+        }
+
+        public void Attach(IntPtr hWndChild)
+        {
+            if (DockingTarget == null)
+            {
+                throw new InvalidOperationException("Cannot attach without DockingTarget specified.");
+            }
+
+            _childHwnd = hWndChild;
+            _parentHwnd = new WindowInteropHelper(ParentWindow).Handle;
+            _originalParentHwnd = SetParent(_childHwnd, _parentHwnd);
+
+            WinStyle[] styles = null;
+
+            if (RemoveDockedWindowBorders)
+            {
+                RemoveBorders(out styles);
+            }
+
+            
+            OriginalParentThread = _originalParentHwnd;
+            OriginalStyles = styles;    
+            DockingTarget.UpdateLayout();
+            DrawWindow();
+
+            if (DockingTarget.IsVisible)
+            {
+                ShowWindow();
+            }
+            else
+            {
+                HideWindow();
+            }
+        }
+
+        public void Detach()
+        {
+            {
+                var hwnd = _childHwnd;
+                _childHwnd = IntPtr.Zero;
+                SetParent(hwnd, OriginalParentThread);
+                RestoreBorders(hwnd, OriginalStyles);
+                ClearChildWindow();
+            }            
+        }
+
+        public void ShowWindow()
+        {
+            if (_isChildVisible)
+            {
+                return;
+            }
+
+            DrawWindow();
+        }
+
+        public void HideWindow()
+        {
+            TrySetWindowState(
+                HWND_NOTOPMOST,
+                WinFlag.HideWindow |
+                    WinFlag.IgnoreMove |
+                    WinFlag.IgnoreResize |
+                    //WinFlag.DoNotRedraw |
+                    WinFlag.DoNotSendChangingEvent |
+                    //WinFlag.DrawFrame |
+                    WinFlag.IgnoreZOrder |
+                    WinFlag.DoNotReposition |
+                    WinFlag.DoNotActivate);
+
+            _isChildVisible = false;
+        }
+
+        public void DrawWindow()
+        {
+            TrySetWindowState(
+                HWND_TOP,
+                WinFlag.DoNotChangeOwnerZOrder |
+                WinFlag.ShowWindow |
+                WinFlag.DrawFrame |
+                WinFlag.AsynchronousWindowPosition |
+                WinFlag.FrameChanged);
+
+            _isChildVisible = true;
+        }
+
+        private void SetWindowVisibility(bool isVisible)
+        {
+            if (isVisible)
+            {
+                ShowWindow();
+            }
+            else
+            {
+                HideWindow();
+            }
+        }
+
+        private void TrySetWindowState(IntPtr insertAfter, WinFlag flags)
+        {
+            try
+            {
+                SetWindowState(insertAfter, flags);
+            }
+            catch (Win32Exception e) when (e.NativeErrorCode == 1400)
+            {
+                _childHwnd = IntPtr.Zero;
+                _isChildVisible = true;
+                _isLoaded = false;
+                RaiseEvent(new RoutedEventArgs(DockedWindowClosedEvent));
+            }
+        }
+
+        private void SetWindowState(IntPtr insertAfter, WinFlag flags)
+        {
+            if (_childHwnd == IntPtr.Zero)
+            {
+                return;
+            }
+
+            if (ParentWindow.IsAncestorOf(DockingTarget))
+            {
+                var pt = DockingTarget.TransformToAncestor(ParentWindow).Transform(new Point(0, 0));
+                _childPos.X = (int)Round(pt.X + Margin.Left);
+                _childPos.Y = (int)Round(pt.Y + Margin.Top);
+                _childPos.Width = (int)Round(DockingTarget.ActualWidth - Margin.Right);
+                _childPos.Height = (int)Round(DockingTarget.ActualHeight - Margin.Bottom);
+
+                ThrowLastErrorIf(
+                    !SetWindowPos(
+                        _childHwnd,
+                        insertAfter,
+                        _childPos.X,
+                        _childPos.Y,
+                        _childPos.Width,
+                        _childPos.Height,
+                        flags));
+            }
+            else
+            {
+                ThrowLastErrorIf(!SetWindowPos(_childHwnd, insertAfter, 0, 0, 0, 0, flags));
+            }
+        }
+
+        public void RemoveBorders(out WinStyle[] styles)
+        {
+            var style = CheckWindowStyle(GetWindowLong(_childHwnd, GWL_STYLE));
+
+            CheckWindowStyle(
+                SetWindowLong(
+                    _childHwnd,
+                    GWL_STYLE,
+                    style &
+                        ~(WS_CAPTION |
+                        WS_THICKFRAME |
+                        WS_MINIMIZEBOX |
+                        WS_MAXIMIZEBOX |
+                        WS_SYSMENU)));
+
+            WinStyle style2;
+
+            if ((style2 = GetWindowLong(_childHwnd, GWL_EXSTYLE)) == None)
+            {
+                styles = new[] { style, None }; 
+
+                return;
+            }
+
+            CheckWindowStyle(
+                SetWindowLong(
+                    _childHwnd,
+                    GWL_EXSTYLE,
+                    style2 &
+                        ~(WS_EX_DLGMODALFRAME |
+                        WS_EX_CLIENTEDGE |
+                        WS_EX_STATICEDGE)));
+
+            styles = new[] { style, style2 };
+        }
+
+        public void RestoreBorders(IntPtr hwnd, WinStyle[] styles)
+        {
+            if (styles == null || styles.Length == 0)
+            {
+                return;
+            }
+
+            if (styles.Length >= 1 && styles[0] != None)
+            {
+                SetWindowLong(hwnd, GWL_STYLE, styles[0]);
+            }
+
+            if (styles.Length == 2 && styles[1] != None)
+            {
+                SetWindowLong(hwnd, GWL_EXSTYLE, styles[1]);
+            }
+        }
+
+        private WinStyle CheckWindowStyle(WinStyle style)
+        {
+            ThrowLastErrorIf(style == None);
+
+            return style;
+        }
+
+        public Process StartProcess(string filename) => StartProcess(filename, null);
+
+        public Process StartProcess(string filename, string arguments)
+        {
+            var pi = arguments != null ?
+                new ProcessStartInfo(filename, arguments) :
+                new ProcessStartInfo(filename);
+
+            pi.UseShellExecute = false;
+            pi.CreateNoWindow = false;
+            pi.WindowStyle = ProcessWindowStyle.Maximized;
+
+            var childProc = Process.Start(pi);
+            var childHwnd = IntPtr.Zero;
+
+            while (childHwnd == IntPtr.Zero)
+            {
+                if (childProc.HasExited)
+                {
+                }
+                else if ((childHwnd = childProc.MainWindowHandle) != IntPtr.Zero)
+                {
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+
+            Attach(childHwnd);
+
+            return childProc;
+        }
+
+        public void FindByClassName(string windowName)
+        {
+            var hWnd = User32.FindWindow(null, windowName);
+
+            if (hWnd != IntPtr.Zero)
+            {
+                Attach(hWnd);
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    $"Could not find window to dock named {windowName}");
+            }
+        }
+
+        private static void DockedProcessPropertyChanged(
+            DependencyObject o,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var ctl = (HwndDockControl)o;
+
+            if (e.OldValue == e.NewValue)
+            {
+                return;
+            }
+
+            if (e.OldValue != null)
+            {
+                ctl.Detach();
+            }
+
+            if (e.NewValue != null)
+            {
+                ctl.Attach(((Process)e.NewValue).MainWindowHandle);
+            }
+        }
+
+        private void ClearChildWindow()
+        {
+            OriginalParentThread = IntPtr.Zero;
+            OriginalStyles = null;
+            _childHwnd = IntPtr.Zero;
+            _parentHwnd = IntPtr.Zero;
+            _originalParentHwnd = IntPtr.Zero;
+            _childPos = new Int32Rect();
+        }
+    }
+}
