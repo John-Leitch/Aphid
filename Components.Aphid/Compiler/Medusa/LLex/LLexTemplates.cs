2d42a06fe5bacedebe07c085b41b3407ebc96487
John Leitch <JohnLeitch@outlook.com>
2019-07-02 19:12:22 -0400

    Compiler updates.

---
+++ Components.Aphid/Compiler/Medusa/LLex/LLexTemplates.cs
@@ -0,0 +1,23 @@
+using global::Components.Aphid.Parser;
+using global::Components.Aphid.Interpreter;
+
+
+namespace Components.Aphid.Compiler.Medusa.LLex
+{
+    using global::Components.Aphid.Lexer;
+    using global::Components.Aphid.Parser;
+    using System.Collections.Generic;
+    
+    public static partial class AphidCompilerResources
+    {
+        public static List<AphidExpression> ByteCode_2e698a1ccb1704c4bc5c5a6c1af959f9()
+        {
+            return             new List<AphidExpression>
+            {
+            }
+;
+        }
+    }
+}
+
+
