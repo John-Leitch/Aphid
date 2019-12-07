b3e06c3786da0f16b2db1f7eeb2667d5bde93cbc
John Leitch <JohnLeitch@outlook.com>
2019-12-06 19:43:19 -0500

    Added parallel command, !# support, and re fix.

--- Components.Aphid/Parser/AphidScript.cs
+++ Components.Aphid/Parser/AphidScript.cs
@@ -6,10 +6,19 @@ namespace Components.Aphid.Parser
 {
     public static class AphidScript
     {
-        public static string Read(string scriptFile) => AphidConfig.Current.ScriptCaching ?
+        public static string Read(string scriptFile)
+        {
+            var script = AphidConfig.Current.ScriptCaching ?
                 Encoding.UTF8
                     .GetString(FileMemoryCache.ReadAllBytes(scriptFile))
                     .Trim(new char[] { '\uFEFF', '\u200B' }) :
                 File.ReadAllText(scriptFile);
+
+            int lineIndex;
+
+            return script.StartsWith("#!") && (lineIndex = script.IndexOf('\n')) != -1 ?
+                script.Substring(lineIndex + 1) :
+                script;
+        }
     }
 }
